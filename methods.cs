029041fba45001038c6c4dda82c1c709ee7f0353
Mark Haines <hainesma@gmail.com>
2021-06-24 16:16:06 -0400

    Create methods.cs

---
+++ methods.cs
@@ -0,0 +1,27 @@
+
+// GetInteger()
+public static int GetInteger(int maxChoices)
+        {
+            string input = "";
+            int output = 0;
+            try
+            {
+                input = Console.ReadLine();
+                output = int.Parse(input);
+                if (output > maxChoices|| output < 1 )
+                {
+                    throw new Exception("That number is out of range. Try again.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That was not a valid input.");
+                output = GetInteger(maxChoices);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                output = GetInteger(maxChoices);
+            }
+            return output;
+        }
